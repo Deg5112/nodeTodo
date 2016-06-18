2a484c58ea7379e9722fd36b564735743f4b568c
David Goodman <deg5112@gmail.com>
2016-06-17 23:40:55 -0700

    list creation done

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,45 @@
 var app = angular.module('todo', []);
 
-app.controller('taskController', function($scope){
+app.controller('listController', function($scope, $http){
     var socket = io.connect();
     console.log(socket);
     var self = this;
+    self.userId = null;
     self.list = [];
-    self.task = {
-        title: null,
-        body: null
+    self.listItem = {
+        listTitle: null
     };
-    self.createTask = function(){
-        socket.emit('createTask', self.task);
+    
+    self.getUserId = function(){
+        $http.get('/userId').then(function(response){
+            self.userId = response.data;
+            self.getList();
+        });
     };
-    socket.on('taskCreationSuccess', function(data){
-        self.list.push(self.task);
-        self.task = {};
+    
+    self.getList = function(){
+        socket.emit('getList', {userId: self.userId});
+    };
+    
+    self.getUserId();
+    
+    self.createList = function(userId){
+        console.log(self.listItem);
+        socket.emit('createListItem', {userId: userId, listItem: self.listItem});
+    };
+    
+    socket.on('getListResponse', function(data){
+        var data = JSON.parse(data);
+        console.log('data from io', data);
+        self.list = data;
         $scope.$digest();
     });
-
+    
+    socket.on('listCreationSuccess', function(data){
+        console.log(data);
+        self.list.push(data.listItem);
+        self.listItem = {};
+        console.log(self.list);
+        $scope.$digest();
+    });
 });
